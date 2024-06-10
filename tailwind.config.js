8cd513983744cfe582c3f08da6d69cd34f7a3f4d
suprasuper <134078308+suprasuper@users.noreply.github.com>
2024-06-11 00:24:30 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,45 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}", "/tailwind.config.js"],
+  theme: {
+    colors: {
+      'vert': {
+        100: '#ECF39E',
+        200: '#90A955',
+        300: '#4F772D',
+        400: '#31572C',
+        500: '#132A13',
+      },
+      'gris' : {
+        100 : '#f1f5f9',
+        200 : '#e2e8f0',
+        300: '#cbd5e1',
+        400: '#94a3b8',
+        500: '#64748b',
+        600: '#475569',
+        700: '#334155'
+      },
+      'blue': '#1fb6ff',
+      'purple': '#7e5bef',
+      'pink': '#ff49db',
+      'orange': '#ff7849',
+      'green': '#13ce66',
+      'yellow': '#ffc82c',
+      'gray-dark': '#273444',
+      'gray': '#8492a6',
+      'gray-light': '#d3dce6',
+    },
+    fontFamily: {
+      sans: ['Graphik', 'sans-serif'],
+      serif: ['Merriweather', 'serif'],
+    },
+    extend: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        pfd: ['Playfair Display', 'serif'],
+      },
+    },
+  },
+  plugins: [],
+}
+
