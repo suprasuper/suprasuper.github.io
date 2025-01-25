a315fa161511e3fc2c249e7828e495dbd83fdcf9
suprasuper <134078308+suprasuper@users.noreply.github.com>
2025-01-25 17:36:31 +0100

    .

---
+++ menumobile.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+    document.getElementById("menu-icon").addEventListener("click", function() {
+        document.querySelector(".nav-links").classList.toggle("active");
+        document.querySelector(".logo").classList.toggle("active");
+    });
+    
+
+
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -39,6 +39,12 @@ module.exports = {
         pfd: ['Playfair Display', 'serif'],
       },
     },
+    screens: {
+      sm: '640px',
+      md: '768px',
+      lg: '1024px',
+      xl: '1280px',
+    },
   },
   plugins: [],
 }
